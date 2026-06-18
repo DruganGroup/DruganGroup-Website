29f2fb55238058373df8ef5b1e0685c4e782305a
BusinessBetter <admin@drugangroup.co.uk>
2026-06-18 16:30:20 +0100

    g

--- check_cols.py
+++
@@ -1,7 +0,0 @@
-import psycopg2, os
-from dotenv import load_dotenv
-load_dotenv()
-conn = psycopg2.connect(os.getenv('DATABASE_URL'))
-cur = conn.cursor()
-cur.execute("SELECT column_name FROM information_schema.columns WHERE table_name='bb_support_tickets'")
-print(cur.fetchall())
--- check_cols_flask.py
+++
@@ -1,9 +0,0 @@
-import sys
-from app import app
-from db import get_db
-
-with app.app_context():
-    conn = get_db()
-    cur = conn.cursor()
-    cur.execute("SELECT column_name FROM information_schema.columns WHERE table_name='bb_support_tickets'")
-    print(cur.fetchall())
